19ad022e539cdf73f51af2a80245067bc272fb19
d1yat <me.sudiyat@gmail.com>
2021-07-03 22:50:38 +0700

    Add initial students

--- src/app.js
+++ src/app.js
@@ -4,6 +4,14 @@ const Student = require('./student');
 const server = http.createServer((request, response) => {
   const { url } = request;
 
+  const students = [
+    new Student(101, 'Ahmad Fasehan'),
+    new Student(102, 'Andris Firmansyah'),
+    new Student(103, 'Akhirudin Sudiyat'),
+    new Student(104, 'Bani Arham'),
+    new Student(105, 'Rido Raudho'),
+  ];
+
   if (url === '/') {
     response.setHeader('Content-Type', 'text/html');
     response.end('Welcome');
