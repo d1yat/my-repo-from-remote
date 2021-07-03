9649930b4a34b514eb3dd19fbe98d1e09014dde7
d1yat <me.sudiyat@gmail.com>
2021-07-03 13:35:55 +0700

    Build simple a Node.js app

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  const { url } = request;
+
+  if (url === '/') {
+    response.setHeader('Content-Type', 'text/html');
+    response.end('Welcome');
+  }
+});
+
+const port = 5000;
+const host = 'localhost';
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
+});
